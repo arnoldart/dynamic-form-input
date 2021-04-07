f5fc2bd36ce2fe3e5f7b33c9b338500c0bfb2102
Arnold Christopher <57179477+arnoldart@users.noreply.github.com>
2021-04-07 14:03:16 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -35,9 +35,7 @@ export default function Home() {
       <div className={tw `container mx-auto`}>
         <h2>Form Mengsedih</h2>
         <form onSubmit={handleSubmit}>
-          {inputFields.map((inputFields, index) => {
-            if(index >= 0) {
-              return (
+          {inputFields.map((inputFields, index) => (
                 <div key={index} className={tw `flex`}>
                 <div>
                   <input className={tw `border-2 mt-5 border-gray-700 mr-2 ${inputFields.isEditable ? "text-gray-800" : "text-gray-500"}`} onChange={event => handleChaneInput(index, event)} name="firstName" placeholder="First Name" value={inputFields.firstName} type="text"/>
@@ -54,9 +52,8 @@ export default function Home() {
                   )}
                 </div>
               </div>
-              )
-            }
-          })}
+            )
+          )}
           <button onClick={handleSubmit} className={tw `p-2 bg-gray-500`}>SEND</button>
         </form>
       </div>
